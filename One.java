d0676737c992e60b27cf597bc32996b2f65b7d46
Filip Krstevski <83398437+filip77487@users.noreply.github.com>
2021-05-13 11:27:21 +0200

    Add files via upload

---
+++ One.java
@@ -0,0 +1,14 @@
+package dayOne;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
+		System.out.println("Hi from Filip");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+	}
+
+}
---
+++ TestTopka.java
@@ -0,0 +1,15 @@
+package dayTwo;
+
+import dayOne.Topka;
+
+public class TestTopka {
+
+	public static void main(String[] args) {
+		Topka a1 = new Topka ();
+//		Topka a2 = new Topka();
+//		System.out.println(a1.name);
+//		System.out.println(a1.size);
+
+	}
+
+}
